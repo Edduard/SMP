1f05032bfb12fa1c9408958d8aa13f39447d7292
SpedsT <spedst@yahoo.com>
2016-04-07 15:31:37 +0300

    Last Commit

--- casa.asm
+++ casa.asm
@@ -8,108 +8,15 @@ w equ 100 ; dimensiune dreptunghi
 code: mov ah, 0
  mov al, 13h ; trecere in mod grafic 320x200
  int 10h  
- 
- 
- ;blueprint
- mov cx, 40
- mov dx, 60
- mov ah, 0ch  
- int 10h
- 
- mov cx, 40
- mov dx, 110
- mov ah, 0ch  
- int 10h
- 
- mov cx, 80
- mov dx, 100
- mov ah, 0ch  
- int 10h
- 
- mov cx, 80
- mov dx, 150
- mov ah, 0ch  
- int 10h
- 
- mov cx, 90
- mov dx, 130
- mov ah, 0ch  
- int 10h
- 
- mov cx, 90
- mov dx, 150
- mov ah, 0ch  
- int 10h
- 
- mov cx, 90
- mov dx, 10
- mov ah, 0ch  
- int 10h
- 
- mov cx, 100
- mov dx, 130
- mov ah, 0ch  
- int 10h  
- 
- mov cx, 100
- mov dx, 150
- mov ah, 0ch  
- int 10h
- 
- mov cx, 110
- mov dx, 110
- mov ah, 0ch  
- int 10h
- 
- mov cx, 110
- mov dx, 150
- mov ah, 0ch  
- int 10h
- 
- mov cx, 130
- mov dx, 50
- mov ah, 0ch  
- int 10h
- 
- mov cx, 140
- mov dx, 110
- mov ah, 0ch  
- int 10h
- 
- mov cx, 140
- mov dx, 150
- mov ah, 0ch  
- int 10h
- 
- mov cx, 170
- mov dx, 90
- mov ah, 0ch  
- int 10h
- 
- mov cx, 110
- mov dx, 150
- mov ah, 0ch 
-  
- int 10h
- mov cx, 180
- mov dx, 100
- mov ah, 0ch  
- int 10h
- 
- mov cx, 180
- mov dx, 150
- mov ah, 0ch  
- int 10h
-
-      
+       
  ; afisare latura superioara
  mov cx, 80+w ; coloana
  mov dx, 100 ; rand
  mov al, 15 ; alb
 u1: mov ah, 0ch ; afisare pixel
- int 10h
- dec cx
- cmp cx, 80
+     int 10h
+     dec cx
+     cmp cx, 80
  jae u1 
  
  ; afisare latura inferioare
@@ -117,9 +24,9 @@ u1: mov ah, 0ch ; afisare pixel
  mov dx, 100+h
  mov al, 15
 u2: mov ah, 0ch
- int 10h
- dec cx
- cmp cx, 80
+     int 10h
+     dec cx
+     cmp cx, 80
  ja u2 
  
  ; latura din stanga
@@ -127,9 +34,9 @@ u2: mov ah, 0ch
  mov dx, 100+h
  mov al, 15
 u3: mov ah, 0ch
- int 10h
- dec dx
- cmp dx, 100
+     int 10h
+     dec dx
+     cmp dx, 100
  ja u3   
  
  ; latura din dreapta
@@ -137,9 +44,9 @@ u3: mov ah, 0ch
  mov dx, 100+h
  mov al, 15 
  u4: mov ah, 0ch
- int 10h
- dec dx
- cmp dx, 100
+     int 10h
+     dec dx
+     cmp dx, 100
  ja u4         
       
 ; latura dreapta acoperis         
@@ -147,10 +54,10 @@ u3: mov ah, 0ch
  mov dx, 100
  mov al, 15
 u5: mov ah, 0ch
- int 10h
- dec cx  
- dec dx
- cmp cx, 130
+     int 10h
+     dec cx  
+     dec dx
+     cmp cx, 130
  jae u5      
  
  ; latura stanga acoperis         
@@ -158,10 +65,10 @@ u5: mov ah, 0ch
  mov dx, 100
  mov al, 15
 u6: mov ah, 0ch
- int 10h
- inc cx  
- dec dx
- cmp cx, 130
+     int 10h
+     inc cx  
+     dec dx
+     cmp cx, 130
  jbe u6  
      
  ;3d stanga jos
@@ -169,10 +76,10 @@ u6: mov ah, 0ch
  mov dx, 100+h
  mov al, 15
 u7: mov ah, 0ch
- int 10h
- dec cx  
- dec dx
- cmp dx, 110
+     int 10h
+     dec cx  
+     dec dx
+     cmp dx, 110
  jae u7 
         
  ;zid spate
@@ -180,9 +87,9 @@ u7: mov ah, 0ch
  mov dx, 110
  mov al, 15
 u8: mov ah, 0ch
- int 10h 
- dec dx
- cmp dx, 60
+     int 10h 
+     dec dx
+     cmp dx, 60
  jae u8 
  
   ;3d stanga sus
@@ -190,10 +97,10 @@ u8: mov ah, 0ch
  mov dx, 100
  mov al, 15
 u9: mov ah, 0ch
- int 10h
- dec cx  
- dec dx
- cmp dx, 60
+     int 10h
+     dec cx  
+     dec dx
+     cmp dx, 60
  jae u9      
  
  ;3d acoperis sus      
@@ -201,10 +108,10 @@ u9: mov ah, 0ch
  mov dx, 50
  mov al, 15
 u10: mov ah, 0ch
- int 10h
- dec cx  
- dec dx
- cmp cx, 90
+     int 10h
+     dec cx  
+     dec dx
+     cmp cx, 90
  jae u10  
  
  ;3d acoperis partea spate
@@ -212,10 +119,10 @@ u10: mov ah, 0ch
  mov dx, 60
  mov al, 15
 u11: mov ah, 0ch
- int 10h
- inc cx  
- dec dx
- cmp cx, 90
+     int 10h
+     inc cx  
+     dec dx
+     cmp cx, 90
  jbe u11  
  
  ;latura stanga usa
@@ -223,9 +130,9 @@ u11: mov ah, 0ch
  mov dx, 150
  mov al, 15
 u12: mov ah, 0ch
- int 10h  
- dec dx
- cmp dx, 130
+     int 10h  
+     dec dx
+     cmp dx, 130
  jae u12   
   
  ;latura dreapta usa
@@ -233,9 +140,9 @@ u12: mov ah, 0ch
  mov dx, 150
  mov al, 15
 u13: mov ah, 0ch
- int 10h  
- dec dx
- cmp dx, 130
+     int 10h  
+     dec dx
+     cmp dx, 130
  jae u13    
   
  ;latura sus usa
@@ -243,9 +150,9 @@ u13: mov ah, 0ch
  mov dx, 130
  mov al, 15
 u14: mov ah, 0ch
- int 10h
- inc cx  
- cmp cx, 100
+     int 10h
+     inc cx  
+     cmp cx, 100
  jbe u14 
  
   ;latura stanga usa mare
@@ -253,9 +160,9 @@ u14: mov ah, 0ch
  mov dx, 150
  mov al, 15
 u15: mov ah, 0ch
- int 10h  
- dec dx
- cmp dx, 110
+     int 10h  
+     dec dx
+     cmp dx, 110
  jae u15   
   
  ;latura dreapta usa mare
@@ -263,9 +170,9 @@ u15: mov ah, 0ch
  mov dx, 150
  mov al, 15
 u16: mov ah, 0ch
- int 10h  
- dec dx
- cmp dx, 110
+     int 10h  
+     dec dx
+     cmp dx, 110
  jae u16    
  
  ;latura jumate usa mare
@@ -273,9 +180,9 @@ u16: mov ah, 0ch
  mov dx, 150
  mov al, 15
 u17: mov ah, 0ch
- int 10h  
- dec dx
- cmp dx, 110
+     int 10h  
+     dec dx
+     cmp dx, 110
  jae u17
  
  ;latura sus usa mare  
@@ -283,9 +190,9 @@ u17: mov ah, 0ch
  mov dx, 110
  mov al, 15
 u18: mov ah, 0ch
- int 10h
- inc cx  
- cmp cx, 170
+     int 10h
+     inc cx  
+     cmp cx, 170
  jbe u18  
    
   ;geam lateral latura stanga
@@ -293,9 +200,9 @@ u18: mov ah, 0ch
  mov dx, 120
  mov al, 15
 u19: mov ah, 0ch
- int 10h
- dec dx 
- cmp dx, 110
+     int 10h
+     dec dx 
+     cmp dx, 110
  jae u19   
       
   ;geam lateral latura dreapta
@@ -303,9 +210,9 @@ u19: mov ah, 0ch
  mov dx, 130
  mov al, 15
 u20: mov ah, 0ch
- int 10h
- dec dx 
- cmp dx, 120
+     int 10h
+     dec dx 
+     cmp dx, 120
  jae u20   
      
   ;geam lateral latura sus
@@ -313,10 +220,10 @@ u20: mov ah, 0ch
  mov dx, 120
  mov al, 15
 u21: mov ah, 0ch
- int 10h
- dec dx 
- dec cx
- cmp dx, 110
+     int 10h
+     dec dx 
+     dec cx
+     cmp dx, 110
  jae u21   
      
   ;geam lateral latura jos
@@ -324,10 +231,10 @@ u21: mov ah, 0ch
  mov dx, 130
  mov al, 15
 u22: mov ah, 0ch
- int 10h
- dec dx 
- dec cx
- cmp dx, 120
+     int 10h
+     dec dx 
+     dec cx
+     cmp dx, 120
  jae u22 
   
  ;latura stanga alee om
@@ -335,11 +242,11 @@ u22: mov ah, 0ch
  mov dx, 150
  mov al, 15
 u23: mov ah, 0ch
- int 10h
- inc dx
- inc dx
- inc cx  
- cmp dx, 200
+     int 10h
+     inc dx
+     inc dx
+     inc cx  
+     cmp dx, 200
  jbe u23   
  
  ;latura dreapta alee om
@@ -347,11 +254,11 @@ u23: mov ah, 0ch
  mov dx, 150
  mov al, 15
 u24: mov ah, 0ch
- int 10h
- inc dx
- inc dx
- inc cx 
- cmp dx, 200
+     int 10h
+     inc dx
+     inc dx
+     inc cx 
+     cmp dx, 200
  jbe u24
  
  ;latura stanga alee masina
@@ -359,10 +266,10 @@ u24: mov ah, 0ch
  mov dx, 150
  mov al, 15
 u25: mov ah, 0ch
- int 10h
- inc dx
- inc cx 
- cmp dx, 200
+     int 10h
+     inc dx
+     inc cx 
+     cmp dx, 200
  jbe u25 
  
  ;latura dreapta alee masina
@@ -370,29 +277,24 @@ u25: mov ah, 0ch
  mov dx, 150
  mov al, 15
 u26: mov ah, 0ch
- int 10h
- inc dx
- inc cx  
- cmp dx, 200
+     int 10h
+     inc dx
+     inc cx  
+     cmp dx, 200
  jbe u26
- 
- ; asteptare apasare tasta
- mov ah,00
- int 16h 
  
 ;User is prompted with the possibility to shut or open the door
 u29:
 PUTC 13 ; linie noua
 PUTC 10 
 PRINTN '1-deschizi usa; 0-inchizi usa '  
-
-PUTC 13   
+PUTC 13  ; linie noua 
 PUTC 10
 CALL scan_num
 PUTC 13
 PUTC 10
         
-mov ax, cx
+mov ax, cx ; pentru ca in program lucram cu registrul cx vom salva valoarea de la tastatura in registrul ax
 
  cmp ax, 0 ; compare the number entered with 0
  jbe u27  ; if below or equals close the door
@@ -418,9 +320,13 @@ u27:
      dec cx 
      cmp cx, 91
      jae u30
-
+     ;play a sound when the door closes
+     mov al, 7
+     mov ah, 0eh   
+     int 10h
+    ;end sound
      
-     jbe u29  ; if already closed do it again
+     jmp u29  ; if already closed prompt the user again
 u28:   
  PUTC 13
  PUTC 10
@@ -434,48 +340,19 @@ u28:
      mov bx, dx
        u33: mov ah, 0ch 
             int 10h
-            dec dx
+            dec dx    
             cmp dx, 130
             jae u33 
      mov dx, bx       
      inc cx 
      cmp cx, 100
      jbe u32
-    ; play a sound when the door opens
-      STARTSOUND2:	;CX=FREQUENCY IN HERTZ. DESTROYS AX & DX
-        CMP CX, 014H
-        JB STARTSOUND2_DONE
-        ;CALL STOPSOUND
-        IN AL, 061H
-        ;AND AL, 0FEH
-        ;OR AL, 002H
-        OR AL, 003H
-        DEC AX
-        OUT 061H, AL	;TURN AND GATE ON; TURN TIMER OFF
-        MOV DX, 00012H	;HIGH WORD OF 1193180
-        MOV AX, 034DCH	;LOW WORD OF 1193180
-        DIV CX
-        MOV DX, AX
-        MOV AL, 0B6H
-        PUSHF
-        CLI	;!!!
-        OUT 043H, AL
-        MOV AL, DL
-        OUT 042H, AL
-        MOV AL, DH
-        OUT 042H, AL
-        POPF
-        IN AL, 061H
-        OR AL, 003H
-        OUT 061H, AL
-        	STARTSOUND2_DONE:
-        
-        	STOPSOUND2:	;DESTROYS AL
-        IN AL, 061H
-        AND AL, 0FCH
-        OUT 061H, AL
-        ;end sound
- jbe u29  ; if already opened do it again           
+    ;play a sound when the door opens
+     mov al, 7
+     mov ah, 0eh   
+     int 10h
+    ;end sound
+ jmp u29  ; if already opened prompt the user again         
   
  
 DEFINE_SCAN_NUM
